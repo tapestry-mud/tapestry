bf626de7fc25919014a829624a194bf4784a1660
Travis Haley <thaley@drivecentric.com>
2026-05-08 14:13:18 -0600

    feat: rewrite drop.js with structured args, bulk support, and mob role

--- packs/tapestry-core/scripts/commands/drop.js
+++ packs/tapestry-core/scripts/commands/drop.js
@@ -1,38 +1,31 @@
 ﻿tapestry.commands.register({
     name: 'drop',
     description: 'Drop an item from your inventory.',
-    priority: 0,
-    handler: function(player, args) {
-        if (args.length === 0) {
-            player.send('Drop what?\r\n');
-            return;
-        }
-        var keyword = args[0];
+    category: 'inventory',
+    roles: ['player', 'mob'],
+    args: {
+        item: { type: 'inventory', required: true, bulk: true }
+    },
+    handler: function(actor, resolved) {
+        var item = resolved.item;
 
-        if (keyword === 'all' || keyword.indexOf('all.') === 0) {
-            var results = tapestry.inventory.dropAll(player.entityId, keyword);
-            if (!results || results.length === 0) {
-                player.send("You aren't carrying anything to drop.\r\n");
+        if (Array.isArray(item)) {
+            if (item.length === 0) {
+                actor.send("You aren't carrying anything to drop.\r\n");
                 return;
             }
-            results.forEach(function(r) {
-                player.send('You drop ' + r.name + '.\r\n');
+            item.forEach(function(i) {
+                tapestry.inventory.drop(actor.entityId, i.keyword);
+                actor.send('You drop ' + i.name + '.\r\n');
             });
-            if (results.length > 0) {
-                player.sendToRoom(player.name + ' drops some items.\r\n');
-            }
+            actor.sendToRoom(actor.name + ' drops some items.\r\n');
             return;
         }
 
-        var found = tapestry.inventory.findByKeyword(player.entityId, keyword);
-        if (!found) {
-            player.send("You aren't carrying that.\r\n");
-            return;
-        }
-        var success = tapestry.inventory.drop(player.entityId, keyword);
+        var success = tapestry.inventory.drop(actor.entityId, item.keyword);
         if (success) {
-            player.send('You drop ' + found.name + '.\r\n');
-            player.sendToRoom(player.name + ' drops ' + found.name + '.\r\n');
+            actor.send('You drop ' + item.name + '.\r\n');
+            actor.sendToRoom(actor.name + ' drops ' + item.name + '.\r\n');
         }
     }
 });
