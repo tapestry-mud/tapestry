3cc2a43159b543453ca66c3a8853b359d9f56076
Travis Haley <thaley@drivecentric.com>
2026-05-08 02:56:15 -0600

    feat: QuestStateRepository - in-memory per-player quest state

---
+++ src/Tapestry.Engine/Quests/QuestStateRepository.cs
@@ -0,0 +1,22 @@
+namespace Tapestry.Engine.Quests;
+
+public class QuestStateRepository
+{
+    private readonly Dictionary<Guid, QuestState> _states = new();
+
+    public QuestState GetOrCreate(Guid playerId)
+    {
+        if (!_states.TryGetValue(playerId, out var state))
+        {
+            state = new QuestState();
+            _states[playerId] = state;
+        }
+        return state;
+    }
+
+    public QuestState? Get(Guid playerId) =>
+        _states.GetValueOrDefault(playerId);
+
+    public IEnumerable<(Guid PlayerId, QuestState State)> All() =>
+        _states.Select(kv => (kv.Key, kv.Value));
+}
