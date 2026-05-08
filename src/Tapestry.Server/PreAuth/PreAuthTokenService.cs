dbc1d431ba2a0d630bb0c52f953ae0645af2c823
Travis Haley <thaley@drivecentric.com>
2026-05-07 23:35:10 -0600

    feat: add PreAuthTokenService for in-memory token store

---
+++ src/Tapestry.Server/PreAuth/PreAuthTokenService.cs
@@ -0,0 +1,39 @@
+using System.Collections.Concurrent;
+
+namespace Tapestry.Server.PreAuth;
+
+public class PreAuthTokenService
+{
+    private readonly ConcurrentDictionary<string, PreAuthToken> _tokens = new();
+    private readonly int _expirySeconds;
+
+    public PreAuthTokenService(int expirySeconds)
+    {
+        _expirySeconds = expirySeconds;
+    }
+
+    public string Issue(string name, PreAuthIntent intent, string? hashedPassword = null)
+    {
+        var token = new PreAuthToken(name, intent, _expirySeconds, hashedPassword);
+        _tokens[token.Id] = token;
+        return token.Id;
+    }
+
+    public PreAuthToken? Consume(string id)
+    {
+        if (!_tokens.TryGetValue(id, out var token))
+        {
+            return null;
+        }
+
+        if (!token.IsValid)
+        {
+            _tokens.TryRemove(id, out _);
+            return null;
+        }
+
+        token.Used = true;
+        _tokens.TryRemove(id, out _);
+        return token;
+    }
+}
